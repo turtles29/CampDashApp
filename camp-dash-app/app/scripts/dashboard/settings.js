dfed07ae3eeb020bb84b0dd0299f14a1a57c200b
abecel22 <abe2211@gmail.com>
2017-09-07 16:15:07 -0500

    Refactored JS and SCSS. HTML adjusted. Added gear animation.

--- camp-dash-app/app/scripts/dashboard/settings.js
+++ camp-dash-app/app/scripts/dashboard/settings.js
@@ -1,11 +1,6 @@
 'use strict';
 
-// $('settings-button').click(function() {
-//     if ($('settings-window').style.display === 'none') {
-
-//     }
-// });
-$( document ).ready(function() {
+$(document).ready(function() {
     $('#settings-window').hide();
     toggleDiv();
     toggleSearch();
@@ -15,47 +10,48 @@ $( document ).ready(function() {
 });
 
 function toggleDiv() {
-    $('#settings-button').click(function() {
-            $('#settings-window').toggle('slow');
+    $('.rotate').click(function() {
+        $(this).toggleClass('down');
+        $('#settings-window').toggle('slow');
     });
 }
 
 function toggleSearch() {
     $('#onoffSearch').click(function() {
-    if( $(this).is(':checked')) {
-        $('#search-container').show();
-    } else {
-        $('#search-container').hide();
-    }
-});
+        if ($(this).is(':checked')) {
+            $('#search-container').show();
+        } else {
+            $('#search-container').hide();
+        }
+    });
 }
 
 function toggleWeather() {
     $('#onoffWeather').click(function() {
-    if( $(this).is(':checked')) {
-        $('#weather-container').show();
-    } else {
-        $('#weather-container').hide();
-    }
-});
+        if ($(this).is(':checked')) {
+            $('#weather-container').show();
+        } else {
+            $('#weather-container').hide();
+        }
+    });
 }
 
 function toggleQuotes() {
     $('#onoffQuotes').click(function() {
-    if( $(this).is(':checked')) {
-        $('#quote-container').show();
-    } else {
-        $('#quote-container').hide();
-    }
-});
+        if ($(this).is(':checked')) {
+            $('#quote-container').show();
+        } else {
+            $('#quote-container').hide();
+        }
+    });
 }
 
 function toggleTodo() {
     $('#onoffTodo').click(function() {
-    if( $(this).is(':checked')) {
-        $('#todo-container').show();
-    } else {
-        $('#todo-container').hide();
-    }
-});
+        if ($(this).is(':checked')) {
+            $('#todo-container').show();
+        } else {
+            $('#todo-container').hide();
+        }
+    });
 }
