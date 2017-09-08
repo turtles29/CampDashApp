c112ecff6052d506baf54a30d5df1f643f4f22b8
awade <newuser@MacBook-3.local>
2017-09-07 22:22:12 -0600

    Added news feed. Not sure what to do yet with the cards it makes.

--- camp-dash-app/app/scripts/dashboard/center.js
+++ camp-dash-app/app/scripts/dashboard/center.js
@@ -3,14 +3,7 @@
 const clockLocation = document.querySelector('#time'),
 greeting = document.querySelector('#greeting'),
 // name
-name = document.querySelector('#username'),
-// form
-fForm = document.querySelector('#focus-form'),
-fInput = document.querySelector('#focus-input'),
-fContainer = document.querySelector('#focusLi'),
-fCheckbox = document.querySelector('#focusCheck'),
-fLabel = document.querySelector('#focus-label'),
-fClose = document.querySelector('#close-button');
+name = document.querySelector('#username');
 
 // Clock stuff
 function updateClock() {
@@ -40,29 +33,3 @@ function getName() {
   if (!localStorage.name) localStorage.name = prompt('What is your name?');
   name.innerHTML = localStorage.name;
 }
-
-document.addEventListener('DOMContentLoaded', function(event) {
-  getName();
-  // Focus stuff
-  if (localStorage.focus) {
-    fContainer.style.display = 'inline-block';
-    fLabel.innerHTML = (localStorage.focus + ' ');
-  } else {
-    fInput.style.display = 'inline-block';
-  }
-
-  fForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    localStorage.focus = fInput.value;
-    fLabel.innerHTML = (localStorage.focus + ' ');
-    // change form
-    fInput.style.display = 'none';
-    fContainer.style.display = 'inline-block';
-  });
-
-  fClose.addEventListener('click', function(e) {
-    localStorage.focus = '';
-    fContainer.style.display = 'none';
-    fInput.style.display = 'inline-block';
-  });
-});
---
+++ camp-dash-app/app/scripts/dashboard/newsfeed.js
@@ -0,0 +1,30 @@
+const NEWS_API_KEY = 'bdff358621fc463bbf3d45c670234c7d';
+
+let settings = {
+	'async': true,
+	'crossDomain': true,
+	'url': 'https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=' + NEWS_API_KEY,
+	'method': 'GET',
+	'headers': {
+		'cache-control': 'no-cache',
+		'postman-token': '18f43ca6-ffe4-6a60-034e-80722e82c891'
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+	let newsContent = response.articles.map(function(article) {
+		return `
+			<a class="article-card" href="${article.url}" target="_blank">
+				<img class="article-img" src="${article.urlToImage}" alt="newsfeed-img" />
+				<div class="article-content">
+					<span class="article-title">${article.title}</span>
+					</div>
+				</a>
+			</a>
+		`;
+	});
+
+	const newsTar = document.querySelector('#newsfeed-container');
+	newsTar.innerHTML = newsContent[0];
+});
